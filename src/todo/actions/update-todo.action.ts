50125ac01f17c35a623d317c816b3a9b5fdbe6c6
andrey_dremin <sankoa230@gmail.com>
2025-04-11 21:07:29 +0300

    refactor: update todo convo

---
+++ src/todo/actions/update-todo.action.ts
@@ -0,0 +1,51 @@
+import { Context, InlineKeyboard } from "grammy";
+import { updateOne } from "../todo.service";
+import { Conversation } from "@grammyjs/conversations";
+
+const askContinueEditing = async (ctx: Context, convo: Conversation) => {
+  const editConfirmationKeyboard = new InlineKeyboard()
+    .text("yes", "todo:edit:yes")
+    .text("no", "todo:edit:no");
+
+  await ctx.reply("Do you wanna continue editing?", {
+    reply_markup: editConfirmationKeyboard,
+  });
+
+  const {
+    callbackQuery: { data },
+  } = await convo.waitForCallbackQuery(["todo:edit:yes", "todo:edit:no"]);
+
+  return data === "todo:edit:yes";
+};
+
+export const handleEditField = async (
+  field: "title" | "description",
+  label: string,
+  ctx: Context,
+  convo: Conversation,
+  todoId: string,
+) => {
+  await ctx.reply(`Write new ${label}!`);
+  const { message, msgId, chatId } = await convo.waitFor(":text");
+
+  const payload =
+    field === "title"
+      ? { content: message?.text }
+      : { description: message?.text };
+
+  const updatedTodo = await updateOne({ id: todoId }, payload);
+
+  const updatedValue =
+    field === "title" ? updatedTodo.content : updatedTodo.description;
+
+  await Promise.all([
+    ctx.api.setMessageReaction(chatId, msgId, [{ type: "emoji", emoji: "👍" }]),
+    ctx.reply(`${updatedValue} - Updated!`, {
+      reply_parameters: {
+        message_id: msgId,
+      },
+    }),
+  ]);
+
+  return await askContinueEditing(ctx,convo);
+};
--- src/todo/coversations/update-todo.conversation.ts
+++ src/todo/coversations/update-todo.conversation.ts
@@ -3,112 +3,57 @@ import { Context } from "grammy";
 import { mainActionsKeyboard } from "../../app/shared/keyboards/main-actions.keyboard";
 import { updateOne } from "../todo.service";
 import { InlineKeyboard } from "grammy";
+import { handleEditField } from "../actions/update-todo.action";
 
 export async function editTodoConvo(
   convo: Conversation,
   ctx: Context,
   todoId: string,
 ) {
-
   let isUserWantsToEdit = true;
-  
+
   while (isUserWantsToEdit) {
+    const editTodoInlineKeyboard = new InlineKeyboard()
+      .text("edit title", "todo:edit:title")
+      .text("edit description", "todo:edit:description")
+      .text("exit", "convo:exit");
 
-  const editTodoInlineKeyboard = new InlineKeyboard()
-    .text("edit title", "todo:edit:title")
-    .text("edit description", "todo:edit:description")
-    .text("exit", "convo:exit");
+    await ctx.reply("Choose what you want to edit", {
+      reply_markup: editTodoInlineKeyboard,
+    });
 
-  await ctx.reply("choose what you want to edit", {
-    reply_markup: editTodoInlineKeyboard,
-  });
+    const {
+      callbackQuery: { data: editTodoChoice },
+    } = await convo.waitForCallbackQuery([
+      "todo:edit:title",
+      "todo:edit:description",
+      "convo:exit",
+    ]);
 
-  const {
-    callbackQuery: { data: editTodoChoice },
-  } = (await convo.waitForCallbackQuery([
-    "todo:edit:title",
-    "todo:edit:description",
-  ])) as {
-    callbackQuery: { data: "todo:edit:title" | "todo:edit:description" };
-  };
+    if (editTodoChoice === "convo:exit") break;
 
-  if (editTodoChoice === "todo:edit:title") {
-    await ctx.reply("write new title!");
-
-    const { message, msgId: messageId, chatId } = await convo.waitFor(":text");
-
-    const updatedTodo = await updateOne(
-      { id: todoId },
-      {
-        content: message?.text,
-      },
-    );
-    const editConfirmationKeyboard = new InlineKeyboard().text("yes","todo:edit:yes").text("no","todo:edit:no")
-
-    await Promise.all([
-      ctx.api.setMessageReaction(chatId, messageId, [
-        { type: "emoji", emoji: "👍" },
-      ]),
-      ctx.reply(`${updatedTodo.content} - Updated! `, {
-        reply_parameters: {
-          message_id: messageId,
-        },
-      }),
-    ])
-
-    await ctx.reply("Do you wanna continue edit?",{
-      reply_markup: editConfirmationKeyboard
-    })
-
-    const { callbackQuery: { data } } = await convo.waitForCallbackQuery(["todo:edit:yes","todo:edit:no"]) as {
-      callbackQuery: {
-        data: "todo:edit:yes" | "todo:edit:no"
-      }
-    }
-    
-    if(data === "todo:edit:no"){
-      isUserWantsToEdit = false
-    }
-  } else {
-    await ctx.reply("write new description!");
-
-    const { message, msgId: messageId, chatId } = await convo.waitFor(":text");
-
-    const updatedTodo = await updateOne(
-      { id: todoId },
-      {
-        description: message?.text,
-      },
-    );
-
-    const editConfirmationKeyboard = new InlineKeyboard().text("yes","todo:edit:yes").text("no","todo:edit:no")
-
-    await Promise.all([
-      ctx.api.setMessageReaction(chatId, messageId, [
-        { type: "emoji", emoji: "👍" },
-      ]),
-      ctx.reply(`${updatedTodo.description} - Updated! `, {
-        reply_parameters: {
-          message_id: messageId,
-        },
-      }),
-    ])
-
-    await ctx.reply("Do you wanna continue edit?",{
-      reply_markup: editConfirmationKeyboard
-    })
-
-    const { callbackQuery: { data } } = await convo.waitForCallbackQuery(["todo:edit:yes","todo:edit:no"]) as {
-      callbackQuery: {
-        data: "todo:edit:yes" | "todo:edit:no"
-      }
-    }
-    
-    if(data === "todo:edit:no"){
-      isUserWantsToEdit = false
+    switch (editTodoChoice) {
+      case "todo:edit:title":
+        isUserWantsToEdit = await handleEditField(
+          "title",
+          "title",
+          ctx,
+          convo,
+          todoId,
+        );
+        break;
+      case "todo:edit:description":
+        isUserWantsToEdit = await handleEditField(
+          "description",
+          "description",
+          ctx,
+          convo,
+          todoId,
+        );
+        break;
     }
   }
-} 
+
   await ctx.reply("Choose Action", {
     reply_markup: mainActionsKeyboard,
   });
